05a6b013819404c05cfb716e62ba5601e56c0269
DinukaDilshan415 <dinukadilshan8026@gmail.com>
2026-08-14 19:19:15 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline border-2 border-amber-600 p-8 rounded-2xl">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
